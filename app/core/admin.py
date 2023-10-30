df64fa06379d9ecfbedb0f4e5819add2b0bb4293
dvirh <dvirhaberman1@gmail.com>
2023-10-30 18:33:59 +0200

    test admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,30 @@
-from django.contrib import admin
+"""
+Dganjo admin customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+
+from core import models
+
+class UserAdmin(BaseUserAdmin):
+    """ Define the admin pages for users """
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        )
+    )
+
+
+admin.site.register(models.User, UserAdmin)
